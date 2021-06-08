189daeee5e1adda5641e5dfa5e2ee658dcd2004b
zakarya01 <zakarya.monsif@gmail.com>
2021-06-08 23:31:39 +0200

    to tomcat9

--- complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -2,9 +2,10 @@ package com.example.securingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecuringWebApplication {
+public class SecuringWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(SecuringWebApplication.class, args);
